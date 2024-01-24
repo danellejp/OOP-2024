134132e51bbf808aa912b1fbca96bf3b22efa5ad
Danelle <C22348731@mytudublin.ie>
2024-01-24 18:17:48 +0000

    Completed Lab 1

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -19,6 +19,12 @@ public class Main
 	{
 		// Put your code here
 		System.out.println("cats are from space");
+
+		Cat ginger = new Cat("Ginger");
+        while (ginger.getNumLives() > 0) {
+            ginger.kill();
+        }
+		
 	}
 	
 	public static void main(String[] args)
